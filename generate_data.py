f0d088f98da3eae293e1d87b96beeb0cd4b93a12
Bogdan Ene <b.ene@nelson.nl>
2022-02-12 17:31:40 +0100

    update generate data

--- generate_data.py
+++ generate_data.py
@@ -15,9 +15,9 @@ def generate_data(files_dir, prefix_dir, nr_files, num_records):
     # if len(res) == 0:
     #     os.mkdir(f'{current_dir}/{files_dir}/{prefix_dir}')
     for i in range(nr_files):
-        cmd = ['./gensort-1.5/gensort', f'-b{i * num_records}', str(num_records), f'{files_dir}/{prefix_dir}/{i}']
+        cmd = ['./gensort-1.5/gensort', f'-b{i * num_records}', str(num_records), f'{files_dir}/{i}']
         subprocess.Popen(cmd, stdout=subprocess.PIPE)
 
 
 if __name__ == '__main__':
-    generate_data('/local/bee700/read_files_generated', '10MB-10files', 10, 100000)
+    generate_data('/local/bee700/read_files_generated/read/', '10MB-10files', 10, 100000)
