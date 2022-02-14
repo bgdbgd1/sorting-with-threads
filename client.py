63e95b355b69957b76ef57ae84560f90a0822e79
Bogdan Ene <b.ene@nelson.nl>
2022-02-14 13:58:08 +0100

    print argv

--- client.py
+++ client.py
@@ -154,5 +154,6 @@ def run_sorting_experiment(experiment_number, nr_files, file_size, intervals, ho
 
 
 if __name__ == '__main__':
+    print(sys.argv)
     for i in range(1, 2):
         run_sorting_experiment(i, '10', '10MB', '256', sys.argv[1], sys.argv[2:])
