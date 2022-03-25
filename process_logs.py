bfe8ccce0751bf23f4b7659e6ba54c24f15b9454
Bogdan Ene <b.ene@nelson.nl>
2022-03-25 14:38:00 +0100

    update nr of threads

--- process_logs.py
+++ process_logs.py
@@ -187,4 +187,4 @@ def process_logs(nr_files, file_size, intervals, pipeline, nr_parallel_threads):
 
 if __name__ == '__main__':
     # process_logs('1000', '100MB', '256', pipeline='pipeline')
-    process_logs('100', '100MB', '256', pipeline='no_pipeline', nr_parallel_threads=8)
+    process_logs('100', '100MB', '256', pipeline='no_pipeline', nr_parallel_threads=24)
