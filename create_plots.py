43bd1532aeace2c8407f1fa6fca56aedae3d2cb7
Bogdan Ene <b.ene@nelson.nl>
2022-04-01 14:30:03 +0200

    16

--- create_plots.py
+++ create_plots.py
@@ -52,7 +52,7 @@ def generate_ecdf(data, dir_name, file_name, xlabel, ylabel):
 
 def create_plots(nr_files, file_size, intervals, pipeline, nr_threads):
     # dir_name =f'logs_determine_bandwidth/logs_nr_files_{nr_files}_file_size_{file_size}_intervals_{intervals}_50_iterations_{nr_threads}_threads_with_rules_50MB_with_distributed_minio'
-    dir_name = 'logs_determine_bandwidth/logs_threads_test/8_threads'
+    dir_name = 'logs_determine_bandwidth/logs_threads_test/8_threads_2'
     with open(f'{dir_name}/results_nr_files_{nr_files}_file_size_{file_size}_intervals_{intervals}_{pipeline}.json', 'r') as results_file:
         experiments_data = json.loads(results_file.read())
 
