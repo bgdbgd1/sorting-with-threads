2369c9be409e78935bb348ae653baf8c3c800944
Bogdan Ene <b.ene@nelson.nl>
2022-04-11 15:33:49 +0200

    upd

--- tc-script2.sh
+++ tc-script2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo /cm/shared/package/utils/bin/run-tc qdisc add dev eth1 root handle 1: prio priomap 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-sudo /cm/shared/package/utils/bin/run-tc qdisc add dev eth1 parent 1:2 handle 20: netem rate 400mbit
-sudo /cm/shared/package/utils/bin/run-tc filter add dev eth1 parent 1:0 protocol ip u32 match ip dport 9000 0xffff flowid 1:2
+sudo /cm/shared/package/utils/bin/run-tc qdisc add dev eth4 root handle 1: prio priomap 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+sudo /cm/shared/package/utils/bin/run-tc qdisc add dev eth4 parent 1:2 handle 20: netem rate 400mbit
+sudo /cm/shared/package/utils/bin/run-tc filter add dev eth4 parent 1:0 protocol ip u32 match ip dport 9000 0xffff flowid 1:2
