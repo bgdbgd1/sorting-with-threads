7fc627ee8861b05f987eeacfc8c7fca68ff87b4b
Bogdan Ene <b.ene@nelson.nl>
2022-05-29 22:21:23 +0200

    update nr of experiments

--- client.py
+++ client.py
@@ -210,5 +210,5 @@ def run_sorting_experiment(experiment_number, nr_files, file_size, intervals, ip
 
 if __name__ == '__main__':
     print(sys.argv)
-    for i in range(1, 11):
+    for i in range(1, 5):
         run_sorting_experiment(i, '100', '1GB', '256', sys.argv[1:])
